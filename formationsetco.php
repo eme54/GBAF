996753dd166c285b49972cd81f093672f95678b0
Emeline Badier <emelinebadier@gmail.com>
2021-05-12 15:32:32 +0200

    creation page acteur 1

---
+++ formationsetco.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  	  <!--Responsive-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Formations&co</title>
+  <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+
+<!--header-->
+<?php include("includes/header-after.php"); ?>
+
+  <section class="acteurs">
+    <img src="formation_co.png" class="logo-acteur-presentation">
+    <div class="texte-presentation">
+    <p>Formation&co est une association française présente sur tout le territoire.
+    Nous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.
+    Notre proposition&thinsp;:</p>
+    <br>
+      <ul>
+      <li>  ●&ensp;&ensp;un financement jusqu’à 30 000€&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;un suivi personnalisé et gratuit&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;une lutte acharnée contre les freins sociétaux et les stéréotypes.</li> 
+    </ul><br> 
+    <p>Le financement est possible, peu importe le métier&thinsp;: coiffeur, banquier, éleveur de chèvres… . Nous collaborons avec des personnes talentueuses et motivées.
+    Vous n’avez pas de diplômes&thinsp;? Ce n’est pas un problème pour nous&thinsp;! Nos financements s’adressent à tous.</p>
+    </div>
+  </section>
+
+<!--footer-->
+<?php include("includes/footer.php"); ?>
+
+</body>
+</html>
