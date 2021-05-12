de2330952e13e17a78b062822563ae4cc47800ca
Emeline Badier <emelinebadier@gmail.com>
2021-05-12 15:26:38 +0200

    creation page accueil

---
+++ accueil.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  	  <!--Responsive-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>GBAF Accueil</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<!--header-->
+<?php include("includes/header-after.php"); ?>
+  
+  <section class="presentation">
+    <h1 class="h1-blanc"> Qui sommes nous&thinsp;? </h1>
+    <div class="texte-presentation"><p><span>Le Groupement Banque Assurance Français</span> (GBAF) est une fédération représentant les 6 grands groupes français&thinsp;:</p><br>
+      <ul>
+      <li>  ●&ensp;&ensp;BNP Paribas&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;BPCE&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;Crédit Agricole&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;Crédit Mutuel-CIC&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;Société Générale&thinsp;;</li> 
+      <li>  ●&ensp;&ensp;La Banque Postale.</li> 
+    </ul><br>
+    <p>Même s’il existe une forte concurrence entre ces entités, elles vont toutes travailler de la même façon pour gérer près de 80 millions de comptes sur le territoire national.<br>
+    Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale. C’est aussi un interlocuteur privilégié despouvoirs publics.</p>
+    </div>
+  </section>
+
+  <section class="acteurs">
+    <h2> Nos acteurs </h2>
+    <div class="bloc-acteur"> 
+      <img src="formation_co.png" class="logo-acteur">
+      <div class="description-acteur">
+        <h3>Formation&co<h3>
+        <p>Formation&co est une association française présente sur tout le territoire. Nous proposons à des personnes issues de tout...</p> 
+        <div class="bloc-bas-acteur">
+          <a class="ensavoirplus" href="Formations&co.html">En savoir plus</a>  
+          <button class="bouton-avis"> <img src="like.svg"></button>
+          <button class="bouton-avis"> <img src="dislike.svg"></button>
+        </div>
+      </div>
+    </div>
+    <div class="bloc-acteur"> 
+      <img src="protectpeople.png" class="logo-acteur">
+      <div class="description-acteur">
+        <h3>Protectpeople<h3>
+        <p>Protectpeople finance la solidarité nationale. Nous appliquons le principe édifié par la Sécurité sociale française en 1945...</p>
+        <div class="bloc-bas-acteur">
+          <a class="ensavoirplus" href="Protectpeople.html"> En savoir plus</a>
+          <button class="bouton-avis"> <img src="like.svg"></button>
+          <button class="bouton-avis"> <img src="dislike.svg"></button>
+        </div>
+      </div>
+    </div>
+    <div class="bloc-acteur"> 
+      <img src="Dsa_france.png" class="logo-acteur">
+      <div class="description-acteur">
+       <h3>Dsa France<h3>
+       <p>Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales. Nous accompagnons les entreprises...</p>
+       <div class="bloc-bas-acteur">
+          <a class="ensavoirplus" href="DSAFrance.html">En savoir plus</a>  
+          <button class="bouton-avis"> <img src="like.svg"></button>
+          <button class="bouton-avis"> <img src="dislike.svg"></button>
+        </div>
+      </div>
+    </div>
+    <div class="bloc-acteur"> 
+      <img src="CDE.png" class="logo-acteur">
+      <div class="description-acteur">
+       <h3>CDE<h3>
+       <p> La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation...</p>
+      <div class="bloc-bas-acteur">
+          <a class="ensavoirplus" href="CDE.html" >En savoir plus</a>  
+          <button class="bouton-avis"> <img src="like.svg"></button>
+          <button class="bouton-avis"> <img src="dislike.svg"></button>
+        </div>
+      </div>
+    </div>
+  </section>
+
+<!--footer-->
+<?php include("includes/footer.php"); ?>
+
+</body>
+</html>
