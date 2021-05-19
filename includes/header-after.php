b69756743dd295b6874b7e365328473faba8aa0a
Emeline Badier <emelinebadier@gmail.com>
2021-05-19 16:47:47 +0200

    mise à jour liens

--- includes/header-after.php
+++ includes/header-after.php
@@ -4,8 +4,8 @@
       <img src="images/icone-utilisateur.svg" alt="icone avatar utilisateur" class="icone-utilisateur">
       <div class="texte-utilisateur">
         <p> Prénom Nom </p> 
-        <a href="compte.html"> Paramètres du compte </a> <br>
-        <a href="page-connexion.html"> Se déconnecter </a> <br>
+        <a href="compte.php"> Paramètres du compte </a> <br>
+        <a href="index.php"> Se déconnecter </a> <br>
       </div>
     </div>
   </header>
