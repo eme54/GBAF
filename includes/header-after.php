afaa5a0873d8d1706c1239f3bab6423db5fd55d2
Emeline Badier <emelinebadier@gmail.com>
2021-05-24 10:44:04 +0200

    affichage nom et prénom de l'utilisateur avec variable de session

--- includes/header-after.php
+++ includes/header-after.php
@@ -1,11 +1,28 @@
+   <?php
+  session_start();
+  //Connexion BDD
+  require '/Users/eme/Documents/GBAF/module/connexion_bdd.php'; 
+  //Requête pour récupérer nom et prénom de l'utilisateur
+  $reponse=$bdd->query('SELECT lastname,name FROM GBAF_account WHERE id_user="'.$_SESSION['id_user'].'"');
+  $donnees=$reponse->fetch()
+   ?>   
    <header>
-  	<img src="images/logo-GBAF.svg" alt="logo du groupement banque assurance français" class="logo-header-gauche">
-    <div class="repere-utilisateur"> 
-      <img src="images/icone-utilisateur.svg" alt="icone avatar utilisateur" class="icone-utilisateur">
-      <div class="texte-utilisateur">
-        <p> Prénom Nom </p> 
+   <img src="images/logo-GBAF.svg" alt="logo du groupement banque assurance français" class="logo-header-gauche">
+   <div class="repere-utilisateur"> 
+     <img src="images/icone-utilisateur.svg" alt="icone avatar utilisateur" class="icone-utilisateur">
+     <div class="texte-utilisateur">
+     <p> <?= htmlspecialchars($donnees['name']);?> <?=htmlspecialchars($donnees['lastname']);?></p> 
         <a href="compte.php"> Paramètres du compte </a> <br>
-        <a href="index.php"> Se déconnecter </a> <br>
+        <a href="actions/deconnexion.php"> Se déconnecter </a> <br>
       </div>
     </div>
   </header>
+  <?php
+  //Fin requête
+  $reponse->closeCursor();
+   ?>
+   
+   
+   
+   
+   
