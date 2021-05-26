4fb96b763b0b217b075d2a4362f6e9deae76eadc
Emeline Badier <emelinebadier@gmail.com>
2021-05-26 17:51:47 +0200

    création page pour changer de mot de passe

---
+++ new_pass.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="utf-8">
+  	  <!--Responsive-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nouveau mot de passe</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<!--header-->
+<?php include("includes/header-before.php"); ?>
+
+<section class="connexion">
+  	<form action="actions/new_pass.php" method="POST"class="champs-connexion">
+  		<p><label for="username">Nom d'utilisateur</label> <br>
+  			<input id="username" type="text" name="username" required ></p> <br>
+
+      	<p><input type="submit" value="Valider"></p>
+
+  		<p>Pas encore de compte ?</p><a href="inscription.php">Inscrivez-vous !</a>
+	
+  	</form>
+  </section>
+
+<?php
+//footer
+include("includes/footer.php");
+?>
+</body>
+</html>
