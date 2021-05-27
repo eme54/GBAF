3026052e4ec5a725ad91f8ce3e7883ae52823fc1
Emeline Badier <emelinebadier@gmail.com>
2021-05-27 15:31:11 +0200

    traduction et révision de la structure

--- actions/new_pass.php
+++ actions/new_pass.php
@@ -1,47 +1,37 @@
 <?php 
+
 session_start();
-//Connexion BDD
+
+//Database connexion
 require '/Users/eme/Documents/GBAF/module/connexion_bdd.php';
 
-//Requête pour verifier existence username dans BDD
+//SQL Request to verify if the username already exists in Database
 $reponse=$bdd->query('SELECT username FROM GBAF_account WHERE username="'.$_POST['username'].'"');
 $username=$reponse->fetch();
 
-if (strtolower($_POST['username'])!=strtolower($username['username']))
-     {
-        $_SESSION['alert']='<p class="message_alert">Ce nom d\'utilisateur n\'existe pas.</p>';
-        header('Location: http://localhost:8888/alert.php');
-     }
-elseif (strtolower($_POST['username'])==strtolower($username['username']))
-     {  
-        //Fin requête
-        $reponse->closeCursor();
+   if (strtolower($_POST['username'])!=strtolower($username['username']))
+   //Alert if the username already exists
+   {
+      $_SESSION['alert']='<p class="message_alert">Ce nom d\'utilisateur n\'existe pas.</p>';
+      header('Location: http://localhost:8888/new_pass.php');
+      exit();
+   }
+   elseif (strtolower($_POST['username'])==strtolower($username['username']))
+   {  
+      //Close Request
+      $reponse->closeCursor();
 
-        $_SESSION['username']=$_POST['username'];
+      $_SESSION['username']=$_POST['username'];
 
-        //Requête pour récupérer question secrète associée dans BDD
-        $reponse=$bdd->query('SELECT question FROM GBAF_account WHERE username="'.$_SESSION['username'].'"');
-        $donnees=$reponse->fetch(); 
-        $_SESSION['question']=$donnees['question'];
-         //Fin requête
-        $reponse->closeCursor(); 
-        header('Location: http://localhost:8888/new_pass_step2.php');
-     }
+      //SQL Request to find the secret question in Database
+      $reponse=$bdd->query('SELECT question FROM GBAF_account WHERE username="'.$_SESSION['username'].'"');
+      $donnees=$reponse->fetch(); 
+      $_SESSION['question']=$donnees['question'];
+         
+      //Close Request
+      $reponse->closeCursor(); 
 
-//Requête pour verifier réponse à la question
-$reponse=$bdd->query('SELECT answer FROM GBAF_account WHERE answer="'.$_POST['answer'].'"');
-$answer=$reponse->fetch();
-if (strtolower($_POST['answer'])!=strtolower($answer['answer']))
-     {
-        $_SESSION['alert']='<p class="message_alert">La réponse est incorrecte.</p>';
-        header('Location: http://localhost:8888/alert.php');
-     }
-elseif ((isset($_POST['answer']))AND(strtolower($_POST['answer'])==strtolower($answer['answer'])))
-     {  
-        //Fin requête
-        $reponse->closeCursor();
-        $_SESSION['answer']=$_POST['answer'];
-        header('Location: http://localhost:8888/new_pass_step3.php');
-     }
-
+      header('Location: http://localhost:8888/new_pass_step2.php');
+      exit();
+   }
 ?>
