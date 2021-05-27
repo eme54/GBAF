a98d2802900b56d8f815d6648c05b34bd4e942c0
Emeline Badier <emelinebadier@gmail.com>
2021-05-27 15:22:16 +0200

    nouvelle décomposition du traitement des étapes pour changer de mot de passe

--- actions/new_pass_step3.php
+++ actions/new_pass_step3.php
@@ -1,27 +1,34 @@
 <?php 
+
 session_start();
 
-//Alerte si erreur dans la création du nouveau mot de passe
+//Alert if the 2 passwords typed don't respect our terms
 if (($_POST['new_password']!=$_POST['new_password2']) OR (!preg_match("#^.{8,}$#", $_POST['new_password'])))
     {
         $_SESSION['alert']='<p class="message_alert"> Les deux mots de passe entrés sont différents ou votre nouveau mot de passe est trop court. </p>';
-        header('Location: http://localhost:8888/alert.php');
+        header('Location: http://localhost:8888/new_pass_step3.php');
+        exit();
     }
-//Si tout est ok
+//If all is ok
 else
-   {//Connexion BDD
-    require '/Users/eme/Documents/GBAF/module/connexion_bdd.php';
-      // Hachage du mot de passe
-      $pass_hache2=password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+   {
+        //Database connexion
+        require '/Users/eme/Documents/GBAF/module/connexion_bdd.php';
 
-      // Requête pour mise à jour du mot de passe dans la table
-      $req=$bdd->prepare('UPDATE GBAF_account SET password=:new_password WHERE username=:username') or die (print_r($bdd->errorinfo()));
-      $req->execute (array(
-          'new_password'=>$pass_hache2,
-          'username'=>$_SESSION['username']
-      ));
-      //Fin requête
-      $req->closeCursor();
-      header('Location: http://localhost:8888/index.php');
+        //Hash password
+        $pass_hache2=password_hash($_POST['new_password'], PASSWORD_DEFAULT);
+
+        //SQL Request to update the password in Database
+        $req=$bdd->prepare('UPDATE GBAF_account SET password=:new_password WHERE username=:username') or die (print_r($bdd->errorinfo()));
+        $req->execute (array(
+            'new_password'=>$pass_hache2,
+            'username'=>$_SESSION['username']
+        ));
+
+        //Close Request
+        $req->closeCursor();
+        
+        header('Location: http://localhost:8888/');
+        exit();
    }
 ?>
