08ab619a684e4cbf996e8ffa04449f1b68edefd6
Emeline Badier <emelinebadier@gmail.com>
2021-06-22 00:10:19 +0200

    modifications pour responsive

--- accueil.php
+++ accueil.php
@@ -31,7 +31,7 @@
             </p>
             <br>
             <ul>
-              <li>  ●&ensp;&ensp;BNP Paribas&thinsp;;</li> 
+              <li>  ●&ensp;&ensp;BNP Paribas&thinsp;;     
               <li>  ●&ensp;&ensp;BPCE&thinsp;;</li> 
               <li>  ●&ensp;&ensp;Crédit Agricole&thinsp;;</li> 
               <li>  ●&ensp;&ensp;Crédit Mutuel-CIC&thinsp;;</li> 
@@ -41,12 +41,10 @@
             <br>
             <p>
             Même s’il existe une forte concurrence entre ces entités, elles vont toutes travailler de la même façon pour gérer près de 80 millions de comptes sur le territoire national.
-            <br>
             Le GBAF est le représentant de la profession bancaire et des assureurs sur tous les axes de la réglementation financière française. Sa mission est de promouvoir l'activité bancaire à l’échelle nationale. 
-            C’est aussi un interlocuteur privilégié despouvoirs publics.
+            C’est aussi un interlocuteur privilégié des pouvoirs publics.
             </p>
           </div>
-
       </section>
 
       <section class="acteurs">
@@ -54,64 +52,60 @@
         <!--Actors list-->
         <h2> Nos acteurs </h2>
       
-          <?php 
-          //SQL Request to show actors
-          //$reponse=$bdd->query('SELECT * FROM GBAF_actor LEFT JOIN GBAF_vote ON GBAF_actor.id_actor = GBAF_vote.id_actor ORDER BY id_actor DESC');
-          $reponse=$bdd->query('SELECT a.*, COUNT(CASE WHEN v.vote = 1 THEN 1 END) AS count_likes, COUNT(CASE WHEN v.vote = 2 THEN 1 END) AS count_dislikes 
-          FROM GBAF_actor AS a LEFT JOIN GBAF_vote AS v ON a.id_actor = v.id_actor GROUP BY a.id_actor ORDER BY a.id_actor DESC');
+          <div class="liste_acteurs">
+            <?php 
+            //SQL Request to show actors
+            //$reponse=$bdd->query('SELECT * FROM GBAF_actor LEFT JOIN GBAF_vote ON GBAF_actor.id_actor = GBAF_vote.id_actor ORDER BY id_actor DESC');
+            $reponse=$bdd->query('SELECT a.*, COUNT(CASE WHEN v.vote = 1 THEN 1 END) AS count_likes, COUNT(CASE WHEN v.vote = 2 THEN 1 END) AS count_dislikes 
+            FROM GBAF_actor AS a LEFT JOIN GBAF_vote AS v ON a.id_actor = v.id_actor GROUP BY a.id_actor ORDER BY a.id_actor DESC');
 
-          while ($donnees=$reponse->fetch())
-          { ?>
-              <div id="<?php $donnees['id_actor']?>" class="bloc-acteur"> 
+            while ($donnees=$reponse->fetch())
+            { ?>
+                <div id="<?php $donnees['id_actor']?>" class="bloc-acteur"> 
 
-                <?=('<img src="images/'.$donnees['logo'].'"class="logo-acteur">')?>
-                
+                  
+                  <?=('<img src="images/'.$donnees['logo'].'"class="logo-acteur">')?>
+                  
+
                   <div class="description-acteur">
                     <h3><?= htmlspecialchars($donnees['actor']);?><h3>
-                      <p><?= substr(($donnees['description']),0,90);?>...</p>
+                      <p><?= substr(($donnees['description']),0,100);?>...</p>
 
                     <div class="bloc-bas-acteur">
 
-                      <a class="ensavoirplus" href="acteur.php?act=<?php echo $donnees['id_actor'];?>">En savoir plus</a>  
-                      <a href="actions/vote.php?act=<?php echo $donnees['id_actor'];?>&type=1" class="bouton-avis" onclick="play()"> <img src="images/like.svg"></a>
+                      <a class="lirelasuite" href="acteur.php?act=<?php echo $donnees['id_actor'];?>">Lire la suite</a>  
 
-                      <?='<p>'.$donnees['count_likes'].'</p>';?>
+                      <a href="actions/vote.php?act=<?php echo $donnees['id_actor'];?>&type=1&redirect=accueil" class="bouton-avis"> <img src="images/like.svg"></a>
 
-                      <a href="actions/vote.php?act=<?php echo $donnees['id_actor'];?>&type=2" class="bouton-avis" onclick="play()"> <img src="images/dislike.svg"></a>
+                      <?='<p class="count">'.$donnees['count_likes'].'</p>';?>
 
-                      <?='<p>'.$donnees['count_dislikes'].'</p>';?>
+                      <a href="actions/vote.php?act=<?php echo $donnees['id_actor'];?>&type=2&redirect=accueil" class="bouton-avis"> <img src="images/dislike.svg"></a>
+
+                      <?='<p class="count">'.$donnees['count_dislikes'].'</p>';?>
+                      <br>
 
                     </div>
 
                   </div>
 
-              </div>
-              <br>
+                </div>
+                <br>
 
-            <?php
-          }
+              <?php
+            }
 
-          //Close Request
-          $reponse->closeCursor();
-          ?>
+            //Close Request
+            $reponse->closeCursor();
+            ?>
 
+          </div> 
+
+      </section> 
 
       <?php
       //footer 
       include_once("includes/footer.php"); 
       ?>
-
-      <!--Animation likes/dislikes script-->
-      <script type="text/javascript">
-        function play() {
-          document.querySelector(".bouton-avis").className = "bouton-avis";
-          window.requestAnimationFrame(function(time) {
-            window.requestAnimationFrame(function(time) {
-              document.querySelector(".bouton-avis").className = "bouton-avis changing";
-            });
-          });
-        }
-      </script>
 
     </body>
 
